3cb16ea325fad46f92bd33e3d57df94a4ae4598c
Masihullah37 <digitalmail372@gmail.com>
2025-10-02 17:57:06 +0200

    controller update

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+// DEPLOYMENT TEST: Last updated via GitHub Actions on October 2, 2024 at 15:48 UTC
+
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
