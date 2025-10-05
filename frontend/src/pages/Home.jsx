3976a4318c602a364527bca6db61b965e5e887ca
Masihullah37 <digitalmail372@gmail.com>
2025-10-05 11:44:09 +0200

    content changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,6 @@ import {useUserContext} from "../context/StudentContext.jsx";
 export default function Home() {
   const context = useUserContext()
   return <>
-    <h1 className={'text-3xl'}>Hi from homepage</h1>
+    <h1 className={'text-3xl'}>welcome to homepage</h1>
   </>
 }
